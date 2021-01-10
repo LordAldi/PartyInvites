668452fcf8e2f949088354cc9c4ec84c8cb238bf
LordAldi <aldianugra09@gmail.com>
2021-01-11 05:03:04 +0700

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using PartyInvites.Models
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            return View();
+
+        }
+    }
+}
